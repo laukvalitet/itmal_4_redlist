541934089b22c7cfa034b8bbdf3cd9fc08f4c2d1
Kristian Lau Jespersen <201807157@post.au.dk>
2021-05-04 17:18:28 +0200

    autosklearn script runs

--- l/autosklearnn.py
+++ l/autosklearnn.py
@@ -10,21 +10,21 @@ from sklearn.preprocessing import LabelEncoder
 
 
 if __name__ == "__main__":
-    eu_red_list_without_unknown_population_trends = pd.read_csv("/Users/kristianjespersen/Documents/IKT/6_semester/ITMAL/itmal_4_redlist/datasets_ready/eu_red_list_without_unknown_population_trends.csv")
+        eu_red_list_without_unknown_population_trends = pd.read_csv("/Users/kristianjespersen/Documents/IKT/6_semester/ITMAL/itmal_4_redlist/datasets_ready/eu_red_list_without_unknown_population_trends.csv")
 
-    encoder = OneHotEncoder(handle_unknown='ignore')
+        X = eu_red_list_without_unknown_population_trends.iloc[:,:-1]
+        y = eu_red_list_without_unknown_population_trends.iloc[:,-1]
+        encoded_X = OneHotEncoder(handle_unknown='ignore').fit_transform(X)
+        
 
-    encoded = encoder.fit_transform(eu_red_list_without_unknown_population_trends)
-
-    print(encoded)
+        encoded_y = LabelEncoder().fit_transform(y)
 
 
-
-    #X_train, X_test, y_train, y_test = \
-    #        sklearn.model_selection.train_test_split(X, y, random_state=1)
-    #automl = autosklearn.classification.AutoSklearnClassifier()
-    #print("Performing fit")
-    #automl.fit(X_train, y_train)
-    #y_hat = automl.predict(X_test)
-    #print("Accuracy score", sklearn.metrics.accuracy_score(y_test, y_hat))
-    #print(automl.show_models())
+        X_train, X_test, y_train, y_test = \
+                sklearn.model_selection.train_test_split(encoded_X, encoded_y, random_state=1)
+        automl = autosklearn.classification.AutoSklearnClassifier()
+        print("Performing fit")
+        automl.fit(X_train, y_train)
+        y_hat = automl.predict(X_test)
+        print("Accuracy score", sklearn.metrics.accuracy_score(y_test, y_hat))
+        print(automl.show_models())
