3ce4948e1485ac2f200cb9955c2ebab3c8d13556
Kristian Lau Jespersen <201807157@post.au.dk>
2021-05-04 16:28:32 +0200

    removed species column since it is different for all animals

---
+++ l/autosklearnn.py
@@ -0,0 +1,30 @@
+from autosklearn.experimental.askl2 import AutoSklearn2Classifier
+import autosklearn
+import sklearn.model_selection
+import sklearn.datasets
+import sklearn.metrics
+from sklearn.datasets import fetch_openml
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+
+
+if __name__ == "__main__":
+    eu_red_list_without_unknown_population_trends = pd.read_csv("/Users/kristianjespersen/Documents/IKT/6_semester/ITMAL/itmal_4_redlist/datasets_ready/eu_red_list_without_unknown_population_trends.csv")
+
+    encoder = OneHotEncoder(handle_unknown='ignore')
+
+    encoded = encoder.fit_transform(eu_red_list_without_unknown_population_trends)
+
+    print(encoded)
+
+
+
+    #X_train, X_test, y_train, y_test = \
+    #        sklearn.model_selection.train_test_split(X, y, random_state=1)
+    #automl = autosklearn.classification.AutoSklearnClassifier()
+    #print("Performing fit")
+    #automl.fit(X_train, y_train)
+    #y_hat = automl.predict(X_test)
+    #print("Accuracy score", sklearn.metrics.accuracy_score(y_test, y_hat))
+    #print(automl.show_models())
